319cff0dd7ad2f5a6e1d8ef510a1332914b1c2a0
dulin <novaline@qq.com>
2015-12-18 12:13:38 +0800

    no message

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -134,4 +134,37 @@
 
     $container.removeClass('mobile-nav-on');
   });
+
+
+    ////firebase访问数量统计
+    //var myFireBase = new Firebase("https://hexoblog.firebaseio.com/");
+    //
+    //var current_url = decodeURI(window.location.pathname.replace(new RegExp('\\/|\\.', 'g'),"_"));
+    //// 获取总数，并将总访问量展示在页面上
+    //myFireBase.child("sum").on("value", function(data) {
+    //    var current_counter = data.val();
+    //    if( $("#sum_counter").length > 0  && current_counter >1 ){
+    //        $("#sum_counter").html(
+    //            "&nbsp;总访问量&nbsp;<span style='color:white'>"+ current_counter +"</span>&nbsp;次"
+    //        );
+    //    };
+    //});
+    //// 获取明细，并将明细也展示在页面上
+    //myFireBase.child("detail/"+current_url).on("value", function(data){
+    //    var detail_counter = data.val();
+    //    if($("#detail_counter").length > 0 && detail_counter > 1){
+    //        $("#detail_counter").html(
+    //            "&nbsp;|&nbsp;本页访问量&nbsp;<span style='color:white'>"+ detail_counter +"</span>&nbsp;次"
+    //        );
+    //    }
+    //});
+    //
+    //// 总数+1
+    //myFireBase.child("sum").transaction(function (current_counter) {
+    //    return (current_counter || 0) + 1;
+    //});
+    //// 明细+1
+    //myFireBase.child("detail/"+current_url).transaction(function (current_counter) {
+    //    return (current_counter || 0) + 1;
+    //});
 })(jQuery);
